a133166efbfb1768cc718fa128a75b2e3d145664
Svetozar <sveto.svensk@gmail.com>
2024-02-24 01:01:32 +0200

    Concater that works

--- ConcatenateAllClipsFromDir.py
+++ ConcatenateAllClipsFromDir.py
@@ -1,28 +1,85 @@
 # This is a sample Python script.
+import os.path
+import random
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from user_classes.ConcaterVidsInFolder import ConcaterVidsInFolder
+from os.path import expanduser
+import argparse
 
-
+EXTENSIONS = ("mp4", "mov", "avi", "vlc", "wmv", "webm", "flv")
+DIRECTORY_DL = "C:\\Users\\User\\Desktop\\Download_Ytb\\ClippedJoined"
 RESOLUTION = "720p"
 VIDEO_CODEC = "libx264"
 VIDEO_QUALITY = "24"
 COMPRESSION = "slow"        # slow, ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
-DIRECTORY_DL = "C:\\Users\\User\\Desktop\\Download_Ytb\\ClippedJoined"
-EXTENSIONS = ("mp4", ".mov", ".avi", ".vlc", ".wmv", ".webm", ".flv")
-SEPARATOR = "\ or /"
+PREF_EXTENSION = ("mp4")
+FINAL_VID_NAME = "GUGUCHI"
 
 
+def get_default_dl():
+    home = expanduser("~")
+    my_dl_location = os.path.join(home, "Desktop", "Download_Ytb", "ClippedJoined")
+    return my_dl_location
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description="From PyVidConcater.sh")
+    parser.add_argument("--dir", type=str)
+    parser.add_argument("--name", type=str)
+    parser.add_argument("--suff", type=str)
+    parser.add_argument("--res", type=str)
+    parser.add_argument("--codec", type=str)
+    parser.add_argument("--qual", type=str)
+    parser.add_argument("--comp", type=str)
+
+    global DIRECTORY_DL, FINAL_VID_NAME, PREF_EXTENSION
+    global RESOLUTION, VIDEO_CODEC, VIDEO_QUALITY, COMPRESSION
+
+    args = parser.parse_args()
+    DIRECTORY_DL = args.dir
+    FINAL_VID_NAME = args.name
+    PREF_EXTENSION = args.suff
+    RESOLUTION = args.res
+    VIDEO_CODEC = args.codec
+    VIDEO_QUALITY = args.qual
+    COMPRESSION = args.comp
+
+    if FINAL_VID_NAME is None:
+        FINAL_VID_NAME = "PyConcaterVid_" + str(random.randint(1, 1000))
+    if PREF_EXTENSION is None:
+        PREF_EXTENSION = "mp4"
+    if RESOLUTION is None:
+        RESOLUTION = "720p"
+    if VIDEO_CODEC is None:
+        VIDEO_CODEC = "libx264"
+    if VIDEO_QUALITY is None:
+        VIDEO_QUALITY = "24"
+    if COMPRESSION is None:
+        COMPRESSION = "slow"
+
+
+def run():
+    global DIRECTORY_DL
+    if DIRECTORY_DL is None:
+        DIRECTORY_DL = get_default_dl()
+    if not os.path.exists(DIRECTORY_DL):
+        os.makedirs(DIRECTORY_DL)
+
+    concater = ConcaterVidsInFolder(DIRECTORY_DL, PREF_EXTENSION, VIDEO_CODEC, VIDEO_QUALITY, COMPRESSION)
+    concater.join_videos(FINAL_VID_NAME)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Remove set_args if running from PyCharm or any other IDE
-    # set_args()
-    concater = ConcaterVidsInFolder(DIRECTORY_DL, EXTENSIONS, VIDEO_CODEC, VIDEO_QUALITY, COMPRESSION)
-    concater.retrieve_vids_from_folder()
+    run_inside_pycharm = os.getenv("RUNNING_IN_PYCHARM")
+    if run_inside_pycharm is None:
+        set_args()
+    run()
+
 
 
 
--- user_classes/ConcaterVidsInFolder.py
+++ user_classes/ConcaterVidsInFolder.py
@@ -1,28 +1,50 @@
 
 from moviepy.editor import *
-import os
+import os, re
 
 
 class ConcaterVidsInFolder:
-    def __init__(self, directory_concat, extensions, video_codec, video_quality, compression):
+    def __init__(self, directory_concat, extension, video_codec, video_quality, compression):
         self.directory_concat = directory_concat
-        self.extensions = extensions
+        self.extension = extension
         self.video_codec = video_codec
         self.video_quality = video_quality
         self.compression = compression
+        self.regex_ext = "(^.*\.(" + extension + ")$)"
+        # self.regex_ext = "(^.*\.("+ '|'.join(extensions) +")$)"   " for array of extensions
 
-    def retrieve_vids_from_folder(self):
+
+    def __retrieve_vids_from_folder(self, flag_present_in_name=""):
+        flag_present_in_name = flag_present_in_name.strip()
+        pattern_ext = re.compile(self.regex_ext)
+        video_list = []
+
         for filename in os.scandir(self.directory_concat):
             if filename.is_file():
-                if filename.endswith():
-                    print(filename.path)
+                if pattern_ext.match(filename.name):
+                    if flag_present_in_name != "":
+                        if flag_present_in_name in filename.name:
+                            full_name = os.path.join(self.directory_concat, filename.name)
+                            video_list.append(full_name)
+                    else:
+                        full_name = os.path.join(self.directory_concat, filename.name)
+                        video_list.append(full_name)
+        return video_list
 
-    def join_videos(self, final_vid_name, flag_in_clip_name=""):
+
+    def join_videos(self, final_vid_name, flag_present_in_name=""):
+        name_list = self.__retrieve_vids_from_folder(flag_present_in_name)
+        print(f" final_vid_name = {final_vid_name}        self.extension = {self.extension}")
+        final_vid_name = final_vid_name + "." + self.extension
+        final_vid_name = os.path.join(self.directory_concat, final_vid_name)
+
+        # concatenate files
         clips_list = []
-
+        for name in name_list:
+            clips_list.append( VideoFileClip(name) )
         final_clip = concatenate_videoclips(clips_list)
+
         # save file
-        final_vid_name = self.directory_concat + self.separator + "Concat_" + "daa"
         final_clip.write_videofile(final_vid_name, threads=4, fps=24,
                                    codec=self.video_codec,
                                    preset=self.compression,
